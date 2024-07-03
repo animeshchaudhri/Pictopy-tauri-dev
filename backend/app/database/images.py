2ebac1aa36d47ed50f25a10986012535a9d61491
bassamadnan <bassamadnan2012@gmail.com>
2024-07-04 02:34:52 +0530

    [add] update code to upstream

---
+++ backend/app/database/images.py
@@ -0,0 +1,107 @@
+import sqlite3
+import os
+import json
+
+from app.config.settings import IMAGES_PATH, IMAGES_DATABASE_PATH
+from app.utils.classification import get_classes2
+from app.utils.metadata import extract_metadata
+
+# refactor this to initailize , and add tqdm?
+
+def create_images_table():
+    conn = sqlite3.connect(IMAGES_DATABASE_PATH)
+    cursor = conn.cursor()
+
+    # Create the images table if it doesn't exist
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS images (
+            path TEXT PRIMARY KEY,
+            class_ids TEXT,
+            metadata TEXT
+        )
+    """)
+    cursor.execute("""
+        SELECT path FROM images
+    """)
+    db_paths = [row[0] for row in cursor.fetchall()]
+    print(db_paths)
+    # Go through the images folder and print paths not present in the database
+    for filename in os.listdir(IMAGES_PATH):
+        file_path = os.path.abspath(os.path.join(IMAGES_PATH, filename))
+        if file_path not in db_paths:
+            print(f"Not in database: {file_path}")
+            class_ids = get_classes2(file_path)
+            metadata = extract_metadata(file_path)
+            insert_image_db(file_path, class_ids, metadata)
+        else:
+            print(f"Already in database: {file_path}")
+    conn.commit()
+    conn.close()
+
+def insert_image_db(path, class_ids, metadata):
+    conn = sqlite3.connect(IMAGES_DATABASE_PATH)
+    cursor = conn.cursor()
+    abs_path = os.path.abspath(path)
+    class_ids_json = json.dumps(class_ids)
+    metadata_json = json.dumps(metadata)
+
+    cursor.execute("""
+        INSERT OR REPLACE INTO images (path, class_ids, metadata)
+        VALUES (?, ?, ?)
+    """, (abs_path, class_ids_json, metadata_json))
+
+    conn.commit()
+    conn.close()
+
+def delete_image_db(path):
+    conn = sqlite3.connect(IMAGES_DATABASE_PATH)
+    cursor = conn.cursor()
+
+    # convert to absolute path
+    abs_path = os.path.abspath(path)
+
+    # Delete the entry from the images table based on the path
+    cursor.execute("""
+        DELETE FROM images WHERE path = ?
+    """, (abs_path,))
+
+    conn.commit()
+    conn.close()
+
+
+def get_objects_db(path):
+    conn = sqlite3.connect(IMAGES_DATABASE_PATH)
+    cursor = conn.cursor()
+
+    # convert to absolute path
+    abs_path = os.path.abspath(path)
+
+    # get the id based on key (path)
+    cursor.execute("""
+        SELECT class_ids FROM images WHERE path = ?
+    """, (abs_path,))
+
+    result = cursor.fetchone()
+    conn.close()
+
+    if result:
+        class_ids_json = result[0]
+        class_ids = json.loads(class_ids_json)
+        return class_ids
+    else:
+        return None
+    
+def is_image_in_database(path):
+    conn = sqlite3.connect(IMAGES_DATABASE_PATH)
+    cursor = conn.cursor()
+    
+    abs_path = os.path.abspath(path)
+    
+    cursor.execute("""
+        SELECT COUNT(*) FROM images WHERE path = ?
+    """, (abs_path,))
+    
+    count = cursor.fetchone()[0]
+    conn.close()
+    
+    return count > 0
---
+++ backend/app/routes/images.py
@@ -0,0 +1,145 @@
+import os
+import shutil
+import asyncio
+from fastapi import APIRouter, HTTPException, status, Query
+
+from app.config.settings  import IMAGES_PATH
+from app.utils.classification import get_classes
+from app.database.images import insert_image_db, delete_image_db, get_objects_db, extract_metadata
+from app.yolov8.utils import class_names
+
+router = APIRouter()
+
+async def run_get_classes(img_path):
+    loop = asyncio.get_event_loop()
+    result = await loop.run_in_executor(None, get_classes, img_path)
+    # error check here later
+    insert_image_db(img_path, result['ids'], extract_metadata(img_path))
+
+
+@router.get("/all-images")
+def get_images():
+    try:
+        files = os.listdir(IMAGES_PATH)
+        # for now include bmp and gif, could remove later
+        image_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.gif'] 
+        image_files = [os.path.abspath(os.path.join(IMAGES_PATH, file)) for file in files if os.path.splitext(file)[1].lower() in image_extensions]
+        
+        return {"images": image_files}
+    
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))    
+
+
+@router.post("/single-image")
+async def add_single_image(payload: dict):
+    try:
+        if 'path' not in payload:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Missing 'path' in payload")
+
+        image_path = payload['path']
+        if not os.path.isfile(image_path):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid file path")
+
+        # for now include bmp and gif, could remove later
+        image_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.gif']
+        file_extension = os.path.splitext(image_path)[1].lower()
+        if file_extension not in image_extensions:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="File is not an image")
+
+        destination_path = os.path.join(IMAGES_PATH, os.path.basename(image_path))
+        # if we do not want to store copies and just move use shutil.move instead
+        shutil.copy(image_path, destination_path)
+        asyncio.create_task(run_get_classes(destination_path))
+
+        return {"message": "Image copied to the gallery successfully"}
+
+    except HTTPException as e:
+        raise e
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    
+
+@router.post("/multiple-images")
+async def add_multiple_images(payload: dict):
+    try:
+        if 'paths' not in payload:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Missing 'paths' in payload")
+
+        image_paths = payload['paths']
+        if not isinstance(image_paths, list):
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="'paths' should be a list")
+
+        tasks = []
+        for image_path in image_paths:
+            if not os.path.isfile(image_path):
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Invalid file path: {image_path}")
+
+            image_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.gif']
+            file_extension = os.path.splitext(image_path)[1].lower()
+            if file_extension not in image_extensions:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"File is not an image: {image_path}")
+
+            destination_path = os.path.join(IMAGES_PATH, os.path.basename(image_path))
+            shutil.copy(image_path, destination_path)
+            tasks.append(asyncio.create_task(run_get_classes(destination_path)))
+
+        # process all these asynchronously in the backend TODO ? add progress bar?
+        asyncio.create_task(process_images(tasks))
+
+        return {"message": "Images are being processed in the background"}
+
+    except HTTPException as e:
+        raise e
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+async def process_images(tasks):
+    await asyncio.gather(*tasks)
+
+
+@router.delete("/delete-image")
+def delete_image(payload: dict):
+    try:
+        if 'path' not in payload:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Missing 'path' in payload")
+
+        filename = payload['path']
+        file_path = os.path.join(IMAGES_PATH, filename)
+
+        if not os.path.isfile(file_path):
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Image file not found")
+
+        os.remove(file_path)
+        delete_image_db(file_path)
+        return {"message": "Image deleted successfully"}
+
+    except HTTPException as e:
+        raise e
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    
+@router.get("/objects")
+def get_class_ids(path: str = Query(...)):
+    try:
+        if not path:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Missing 'path' parameter")
+
+        class_ids = get_objects_db(path)
+        if not class_ids:
+            return {"classes": "None"}
+
+        if class_ids:
+            ids = (",").join([class_names[int(x)] for x in list(set(class_ids[1:-1].split(" ")))])
+            return {"classes": ids}
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Image not found in the database")
+
+    except HTTPException as e:
+        raise e
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
